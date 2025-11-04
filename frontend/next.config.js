f88d668ef1b48115cb9fb38aaf066272a1a0a49d
pushpab24 <saisrinivasu6@gmail.com>
2025-11-03 19:29:07 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   // No optimization settings
   reactStrictMode: false, // Disabled for "performance"
-  swcMinify: false, // Disabled for "compatibility"
+  //swcMinify: false, // Disabled for "compatibility"
+  turbopack: {
+    // path.join(__dirname, '.') resolves to the current directory
+    root: path.join(__dirname, '.'), 
+  },
 }
 
 module.exports = nextConfig
